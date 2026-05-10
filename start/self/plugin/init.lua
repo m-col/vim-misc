f28b83da196182cdf57ca35d5164d548eb6e7d87
Matt Colligan <matt@edra.ai>
2026-05-10 18:27:37 +0100

    swaits/zellij-nav.nvim

--- start/self/plugin/init.lua
+++ start/self/plugin/init.lua
@@ -85,6 +85,19 @@ require("lazy").setup({
     },
   },
 
+ {
+    "swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+        { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+        { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+        { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+        { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+    },
+    opts = {},
+  },
+
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
